693d837fcd453116bb1046922ecdc0453d7db4c2
T-Streets <tyler.ceja5@gmail.com>
2018-09-20 00:34:33 -0700

    adds route for form api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.post('/api/form', (req, res) => {
     console.log(req.body)
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
