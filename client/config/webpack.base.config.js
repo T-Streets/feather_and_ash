08a755b32f72bd3cc408ffb7ba6e2965e2c6e640
Daniel Zuzevich <dzuzevich@graftonstudio.com>
2018-09-20 16:38:25 -0400

    fixed api problem

--- client/config/webpack.base.config.js
+++ client/config/webpack.base.config.js
@@ -1,5 +1,13 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const API_URL = {
+    production: JSON.stringify('https://somebaseproductionurl.com'),
+    development: JSON.stringify('http://localhost:3000/api')
+}
+
+const environment = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = {
     module: {
@@ -60,6 +68,9 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: 'style.css'
+        }),
+        new webpack.DefinePlugin({
+            'API_URL': API_URL[environment]
         })
     ]
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const nodeMailer = require('nodemailer');
 const app = express();
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
 
 app.post('/api/form', (req, res) => {
     console.log(req.body)
