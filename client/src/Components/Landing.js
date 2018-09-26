18d51868f2f310f3c8da6008b31240ea0fce0f3a
T-Streets <tyler.ceja5@gmail.com>
2018-09-26 00:16:39 -0700

    adds logo to landing + nav component

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Logo from '../images/FA-logo-black.png';
 
 const Landing = () => {
     return (
         <div className = 'landingContainer'>
             <h1>Feather <span className='ampersand'>&</span> Ash</h1>
             <h2 className='title'>Productions</h2>
+            <img className='landingLogo' src={Logo} width='200px' height='200px' />
+
         </div>
     )
 }
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../images/FA-logo-black.png'
 
 const Nav = () => {
     return (
         <nav>
             <ul className='container'>
-                <li className='logo'>Logo</li>
+                <img className='Logo' src={Logo} width='60px' height='60px' />
                 <a href='#portfolio'><li>Portfolio</li></a>
                 <a href='#form'><li>Quote</li></a>
             </ul>
