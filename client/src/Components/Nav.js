9578f65e975686b9970ca85190b2a448917063a1
T-Streets <tyler.ceja5@gmail.com>
2018-09-26 17:48:33 -0700

    edits nav

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -5,7 +5,6 @@ const Nav = () => {
     return (
         <nav>
             <ul className='container'>
-                <img className='Logo' src={Logo} width='60px' height='60px' />
                 <a href='#portfolio'><li>Portfolio</li></a>
                 <a href='#form'><li>Quote</li></a>
             </ul>
