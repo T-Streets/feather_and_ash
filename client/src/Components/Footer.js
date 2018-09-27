0d72623c36bfe657c395491231120ce1f7555eec
T-Streets <tyler.ceja5@gmail.com>
2018-09-26 17:45:03 -0700

    adds clickable icons to footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return (
         <div className='footer'>
+            <div >
+                <a href='mailto:tyler@featherandash.io'>
+                    <FontAwesomeIcon icon='envelope' color='white' className='iconMail' />
+                </a>
+                <a href='https://github.com/T-Streets' target='_blank'>
+                    <FontAwesomeIcon icon={['fab','github']} color='white' className='iconGit'/>
+                </a>
+            </div>
         </div>
     )
 }
