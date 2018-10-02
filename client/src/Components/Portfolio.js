bfacbe5917954136eef06bb7a1ada371e1ec2fa7
T-Streets <tyler.ceja5@gmail.com>
2018-10-01 23:58:33 -0700

    adds media queries + changes fonts

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = () => {
         <div id='portfolio' className='portfolioContainer'>
             <h2 className='portHeader'>Take a look at some of our most recent creations</h2>
             <hr className='line-break'/>
-            <div className='portfolioImages'>
+            <div className='portfolioImageContainer'>
                 <a href='http://goodomens.com/' target='_blank'><img src={OG} className='portImages'/></a>
                 <a href='https://sacredstonelegacy.com/'  target='_blank'><img src={Sacred} className='portImages'/></a>
                 <a href='http://goddessnest.com/'  target='_blank'><img src={Goddess} className='portImages'/></a>
