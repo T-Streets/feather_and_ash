d10959042a72feb44dac382c8e61cfbbb715fc0a
T-Streets <tyler.ceja5@gmail.com>
2018-10-01 23:20:44 -0700

    sets media queiries for tablet + mobile resolutions

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -4,14 +4,14 @@ import '../../scss/about.scss';
 const About = () => {
     return (
         <div className='aboutContainer'>
-            <hr />
-            <p><span className='brand'>Feather & Ash Productions</span> strives to be a leader in not only building beautiful and unique web applications,
+            <p>
+                <span className='brand'>Feather & Ash Productions</span> strives to be a leader in not only building beautiful and unique web applications,
                 but also in establishing real, genuine relationships. Our goal is to simply make your ideas come alive.
                 <br/> 
                 <br/> 
                 The web is your office, your playground, your voice. <span className='brand'>Feather & Ash Productions</span> will make your voice heard. Click below to request a quote for your idea 
                 waiting to be born!
-                 </p>
+            </p>
                  <br />
                  <a href='#form'><button className='quoteButton'> Request Quote</button></a>
         </div>
